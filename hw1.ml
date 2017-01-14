2ace59961e0fa8804fa199463fbac757ca891dab
Alex Omar <omat112694@gmail.com>
2017-01-14 12:11:12 -0800

    adding more hw functions

--- hw1.ml
+++ hw1.ml
@@ -5,7 +5,7 @@
 (* Returns a list with one element matching e removed *)
 let rec remove e l =
     match l with
-      [] -> l
+      []     -> l
     | hd::tl -> if hd=e
                 then tl
                 else hd :: (remove e tl);;
@@ -13,21 +13,38 @@ let rec remove e l =
 (* True if a list l contains an element e *)
 let rec contains e l =
     match l with
-      [] -> false
+      []     -> false
     | hd::tl -> if hd=e
                 then true
                 else (contains e tl);;
 
 
+(*
+ * HW FUNCTION - subset
+ *
+ * remove the first element of a from both a and b, then recurse.
+ * if that element does not exist in b then a is not a subset.
+ * *)
 let rec subset a b = 
     match a with
-      [] -> true
+      []     -> true
     | hd::tl -> if (contains hd b)
                 then let bb = (remove hd b) in
                     subset tl bb
                 else false;;
-
-
+(*
+ * HW FUNCTION - equal_sets
+ *
+ * remove the first element of a from b, then recurse again
+ * but pass in the two modified lists.
+ * *)
+let rec equal_sets a b =
+    match a with
+      []     -> if b=[] then true else false
+    | hd::tl -> if contains hd b
+                then let bb = remove hd b in
+                  equal_sets tl bb
+                else false;;
 
 
 
